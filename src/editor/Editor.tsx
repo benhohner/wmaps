f600673583906787847b5d1f0c333070679975f5
Ben Hohner <benhohner@gmail.com>
2022-08-25 14:51:27 -0400

    fix async call for browsers

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -8,7 +8,9 @@ import { updateEditorText } from "../state/State";
 // EDITOR
 export const editor = Monaco.editor.create(document.getElementById("editor")!, {
   value:
-    (await localforage.getItem("editorText")) ||
+    (async () => {
+      await localforage.getItem("editorText");
+    })() ||
     "component client [0.99, 0.45]\ncomponent wellbeing [0.91, 0.67]\ncomponent emotional expression [0.69, 0.25] \ncomponent healthy belief systems [0.69, 0.68] \ncomponent habits [0.47, 0.56] \ncomponent exercise [0.29, 0.53]\ncomponent diet [0.31, 0.63]\ncomponent self care [0.27, 0.41]\nclient->wellbeing\nwellbeing->emotional expression\nwellbeing->healthy belief systems\nwellbeing->habits\nhabits->diet\nhabits->exercise\nhabits->self care",
   language: "javascript",
   theme: "vs-dark",
