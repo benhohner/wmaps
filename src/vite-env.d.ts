579d8e4d65b6c6d6b21a19d5eb4a76752f4a6e5e
Ben Hohner <benhohner@gmail.com>
2022-09-28 15:43:42 -0400

    add production config for heap and support for env vars in html

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_DEBUG_ENABLED: string;
+  readonly VITE_HEAP_ID: string;
   // put vite env variables here to allow intellisense
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { mergeConfig } from "vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { defineConfig as dc } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
@@ -13,9 +13,28 @@ const testConfig = dc({
   },
 });
 
-export default mergeConfig(
-  testConfig,
-  defineConfig({
-    plugins: [react()],
-  })
-);
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  // Plugin to enable replacing %VITE_ENV_VAR% syntax with the VITE_ENV_VAR variable in html
+  const htmlEnvVariables = () => {
+    return {
+      name: "html-env-variables",
+      transformIndexHtml(html: string) {
+        return html.replace(/%(.*?)%/g, function (match, p1) {
+          if (env[p1]) {
+            return env[p1].replace(/$[\"\']|[\"\']^/, "");
+          } else {
+            throw new Error(
+              `Vite htmlEnvVariables Error: the environment variable ${p1} is not defined. Check if the variable name is misspelled in your HTML, or if the variable is not defined in your ${mode} environment.`
+            );
+          }
+        });
+      },
+    };
+  };
+
+  return mergeConfig(testConfig, {
+    plugins: [htmlEnvVariables(), react()],
+  });
+});
